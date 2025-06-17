339f9e577011312ae01f8dd63746bee8f0bc8822
anisaa-marlina <anisaamarlinaa@gmail.com>
2025-06-17 13:57:52 +0700

    add 11-ikon keranjang

--- js/script.js
+++ js/script.js
@@ -1,16 +1,46 @@
-// Toggle classs active
+// Toggle class active
 const navbarNav = document.querySelector(".navbar-nav");
 
-// ketika hamburger menu di klik
-document.querySelector("#hamburger-menu").onclick = () => {
+// ketika hamburger menu diklik
+const hm = document.querySelector("#hamburger-menu");
+hm.onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
-// klik di luar sidebar untuk menghilangkan nav
-const hamburger = document.querySelector("#hamburger-menu");
+// Toggle class active untuk search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+const sb = document.querySelector("#search-button");
 
+sb.onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// Toggle class active untuk shopping cart
+const shoppingCart = document.querySelector(".shopping-cart");
+const sc = document.querySelector("#shopping-cart-button");
+
+sc.onclick = (e) => {
+  shoppingCart.classList.toggle("active");
+  e.preventDefault();
+};
+
+// Klik di luar elemen
 document.addEventListener("click", function (e) {
-  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+  // untuk navbar
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+
+  // untuk search form
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
+
+  // untuk shopping cart
+  if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove("active");
   }
 });
