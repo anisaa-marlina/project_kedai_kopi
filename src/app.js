5d5fc32f21f226a2d2f8f277f6859863b626a8fe
anisaa-marlina <anisaamarlinaa@gmail.com>
2025-06-25 10:43:38 +0700

    22-menampilkan gambar produk secara dinamis dari app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 document.addEventListener("alpine:init", () => {
   Alpine.data("products", () => ({
     items: [
-      { id: 1, name: "Robusta Brazil", img: "1.jpg", price: 20000 },
+      { id: 1, name: "Robusta Brazil", img: "1.jpeg", price: 20000 },
       { id: 2, name: "Arabica Blend", img: "2.jpg", price: 25000 },
       { id: 3, name: "Primo Passo", img: "3.jpg", price: 30000 },
       { id: 4, name: "Aceh Gayo", img: "4.jpg", price: 35000 },
